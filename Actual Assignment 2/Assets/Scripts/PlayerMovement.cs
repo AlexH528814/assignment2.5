dca533e33679bcbb0f244c0878e9f4e5fb3fa239
AlexH528814 <528814@escg.ac.uk>
2022-10-19 21:15:14 +0100

    update
    animations mostly work, and I slide down most of the terrain sides instead of sticking to it, but i also sometimes go inside the terrain and i dont know why

--- Actual Assignment 2/Assets/Scripts/PlayerMovement.cs
+++ Actual Assignment 2/Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpGround;
+
+
     public float speed = 10f;
     public float jump = 10f;
+
+    public int doublejump = 0;
+
+    private float dirX = 0f;
+    
+    private enum MovementState {idle, running, jumping }
+    private MovementState state = MovementState.idle;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
+        Debug.Log(dirX);
         
         rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
-       if (Input.GetButtonDown("Jump"))
+       if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jump);
+            
+            
+        }
+        UpdateAnimationState();
+    }   
+
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true); 
+            sprite.flipX = false;
         }
 
-        
+        if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
 
+        if (dirX == 0f)
+        {
+            anim.SetBool("running", false);
+        }
+
+        if (isGrounded() == false)
+        {
+            anim.SetBool("jumping", true);
+        }
+
+        if (isGrounded() == true)
+        {
+            anim.SetBool("jumping", false);
+        }
+    }
+    
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpGround);
     }
 }
